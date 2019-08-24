29275508df99d46c1a219a013f173402de3cb457
kimbagira Sylvie <sylviekimbagira@gmail.com>
2019-08-24 22:54:14 +0200

    added some changes

--- src/app/quotee/quotee.component.ts
+++ src/app/quotee/quotee.component.ts
@@ -11,6 +11,16 @@ export class QuoteeComponent implements OnInit {
     new Quotes(1, 'kimbagira', 'sylvie', 'Write it on your heart that every day is the best day in the year.',new Date(2019,1,3)),
    ];
 
+   addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+
+
+
+
    toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
